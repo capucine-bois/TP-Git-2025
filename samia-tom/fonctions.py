632cd840b0370505724d1872c8a25a000a4b7e46
Samia <samiabbenjelloun@gmail.com>
2025-12-11 14:58:50 +0100

    jsp

--- samia-tom/fonctions.py
+++ samia-tom/fonctions.py
@@ -14,11 +14,13 @@ def noms_binome():
     Affiche les noms des membres du binôme
     Attention, chacun écrit la ligne pour afficher son nom
     """
+    print("Tom")
     print("Samia")
 
 
 a = 2
 b = 1
+
 print(f"La somme de {a} et {b} vaut {addition(a, b)}")
 print(f"La différence de {a} et {b} vaut {soustraction(a, b)}")
 
